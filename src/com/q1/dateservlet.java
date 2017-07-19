9260300f660604fa496ca95a7434ecd5759b688b
diablo <diablo@diablo-Latitude-E5520>
2017-07-19 16:11:16 +0530

    assgn 5.2

---
+++ src/com/q1/dateservlet.java
@@ -0,0 +1,37 @@
+package com.q1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class dateservlet
+ */
+@WebServlet("/dateservlet")
+public class dateservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setIntHeader("Refresh", 10);
+		PrintWriter pw = response.getWriter();
+		pw.println("time is "+new Date());
+		
+		
+			
+				
+				
+		
+		
+		
+	}
+
+}
---
+++ src/com/q2/demo.java
@@ -0,0 +1,42 @@
+package com.q2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class demo
+ */
+@WebServlet("/demo")
+public class demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String ss = request.getParameter("usrname");
+		
+		PrintWriter pw = response.getWriter();
+		pw.println("hi "+ss+" from get");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String ss = request.getParameter("usrname1");
+		
+		PrintWriter pw = response.getWriter();
+		pw.println("hi "+ss+" from post");
+	}
+
+}
